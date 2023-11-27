fc15e0fffbfc332934641aa70ed9d6595cc4a37f
Shahad842 <shahad_9215@hotmail.com>
2023-11-27 11:43:52 +0300

    Initial Commit

---
+++ MC2TEAM2project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC2TEAM2project
+//
+//  Created by Shahad Alhothali on 13/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC2TEAM2project/MC2TEAM2projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2TEAM2projectApp.swift
+//  MC2TEAM2project
+//
+//  Created by Shahad Alhothali on 13/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MC2TEAM2projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
